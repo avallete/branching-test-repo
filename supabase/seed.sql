7161ed966a97db3a765188775bf64cd2c7903f20
avallete <andrew.valleteau@supabase.io>
2024-09-10 10:07:46 +0200

    init

---
+++ supabase/seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO projects (name, description) VALUES ('Project 1', 'Description 1');
+INSERT INTO projects (name, description) VALUES ('Project 2', 'Description 2');
+INSERT INTO projects (name, description) VALUES ('Project 3', 'Description 3');
